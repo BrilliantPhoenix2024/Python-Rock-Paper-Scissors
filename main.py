74c3f3020e46615f0cd5adf72a0b399b791aa717
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-08-14 23:05:33 +0330

    Implementation Random Choice for AI

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
+import random
+
 user_choice = input("Select from Rock, Paper, Scissors: (r, p, s) ")
 
+choice_meaning = {
+    'r':"Rock",
+    'p':"Paper",
+    's':"Scissors"
+}
+
 choices = ['r', 'p', 's']
-ai_choice = 'r'
+ai_choice = random.choice(choices)
 
 if user_choice in choices:
-    print(f"You chose {user_choice} and AI chose {ai_choice}.")
+    print(f"Your choice is {choice_meaning[user_choice]} and AI choice is {choice_meaning[ai_choice]}.")
     # r > s - p > r - s > p
     if user_choice == ai_choice:
         print('Tie')
-    elif (user_choice == 'r') and (ai_choice == 's'):
+    elif ((user_choice == 'r') and (ai_choice == 's')) or ((user_choice == 'p') and (ai_choice == 'r')) or ((user_choice == 's') and (ai_choice == 'p')):
         print('you Won!')
-    elif (user_choice == 'p') and (ai_choice == 'r'):
-        print('You Won!')
-    elif (user_choice == 's') and (ai_choice == 'p'):
-        print('You Won!')
     else:
         print("You Lost!")
 else:
