efcebb4c65fbac4554afbb0ff7605f31ea9dcf60
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-08-17 18:19:05 +0330

    ADD 'for Loop' to the game

--- main.py
+++ main.py
@@ -1,24 +1,34 @@
 import random
 
-user_choice = input("Select from Rock, Paper, Scissors: (r, p, s) ")
+choices = ['r', 'p', 's']
 
 choice_meaning = {
-    'r':"Rock",
-    'p':"Paper",
-    's':"Scissors"
-}
+        'r':"Rock",
+        'p':"Paper",
+        's':"Scissors"
+    }
 
-choices = ['r', 'p', 's']
-ai_choice = random.choice(choices)
+user_score = 0
+ai_score = 0
+    
+for i in range(5):
+    user_choice = input("Select from Rock, Paper, Scissors: (r, p, s) ")
+    
+    ai_choice = random.choice(choices)
 
-if user_choice in choices:
-    print(f"Your choice is {choice_meaning[user_choice]} and AI choice is {choice_meaning[ai_choice]}.")
-    # r > s - p > r - s > p
-    if user_choice == ai_choice:
-        print('Tie')
-    elif ((user_choice == 'r') and (ai_choice == 's')) or ((user_choice == 'p') and (ai_choice == 'r')) or ((user_choice == 's') and (ai_choice == 'p')):
-        print('you Won!')
+    if user_choice in choices:
+        print(f"Your choice is {choice_meaning[user_choice]} and AI choice is {choice_meaning[ai_choice]}.")
+        # r > s - p > r - s > p
+        if user_choice == ai_choice:
+            print('Tie')
+        elif ((user_choice == 'r') and (ai_choice == 's')) or ((user_choice == 'p') and (ai_choice == 'r')) or ((user_choice == 's') and (ai_choice == 'p')):
+            print('You Won!')
+            user_score += 1
+        else:
+            print("AI Won!")
+            ai_score += 1
     else:
-        print("You Lost!")
-else:
-    print('Invalid input')
+        print('Invalid input')
+
+    print(f'User Score:{user_score} | AI Score:{ai_score}')
+    print('\n', '-'*15, '\n')
